f655eab4d15dc966a762dffcd11fc235fb00a2c3
FERRALI-Mario <mario.ferrali@etu.univ-amu.fr>
2022-02-23 09:14:59 +0100

    Model

--- cv/models.py
+++ cv/models.py
@@ -1,42 +1,44 @@
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 
+# photo = FileSystemStorage(location='templates/img')
+
+
 class Profil(models.Model):
-    image = models.ImageField(),
-    first_name = models.CharField(max_length=30),
-    last_name = models.CharField(max_length=30),
-    description = models.TextField(max_length=2000),
+    # image = models.ImageField(storage=photo)
+    image = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    description = models.TextField(max_length=2000)
 
 
 class Competencie(models.Model):
-    PERCENTAGE = (
+    PERCENTAGE = {
         ('60', 'Little'),
         ('80', 'Medium'),
         ('100', 'Lot'),
-    ),
-    name = models.CharField(max_length=100),
-    percentage = models.CharField(max_length=1, choices=PERCENTAGE),
+    }
+    name = models.CharField(max_length=100)
+    percentage = models.CharField(max_length=10, choices=PERCENTAGE)
 
 
 class Experience(models.Model):
-    post = models.CharField(max_length=100),
-    company = models.CharField(max_length=100),
-    date_start = models.DateField(),
-    date_end = models.DateField(),
-    description = models.TextField(max_length=2000),
+    post = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    date_start = models.DateField(auto_now_add=False)
+    date_end = models.DateField(auto_now_add=False)
+    description = models.TextField(max_length=2000)
 
 
 class Formation(models.Model):
-    name = models.CharField(max_length=100),
-    location = models.CharField(max_length=100),
-    date_start = models.DateField(),
-    date_end = models.DateField(),
-    description = models.TextField(max_length=2000),
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    date_start = models.DateField(auto_now_add=False)
+    date_end = models.DateField(auto_now_add=False)
+    description = models.TextField(max_length=2000)
 
 
 class Contact(models.Model):
-    icon = models.CharField(max_length=100),
-    title = models.CharField(max_length=100),
-
-
-
+    icon = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
