6ab2c219712a18f61b18490986a7e8a5075ff783
FERRALI-Mario <mario.ferrali@etu.univ-amu.fr>
2022-02-23 09:36:33 +0100

    Model Profil

--- cv/models.py
+++ cv/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Profil(models.Model):
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+    year = models.DateField(auto_now=False, auto_now_add=False)
+    license = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    description = models.TextField(max_length=2000)
